92336a5b10a65c309626563dd4b4120799d2e728
termeh <termehtabatabaee@yahoo.com>
2021-07-07 13:52:49 +0430

    Add project files.

---
+++ marketing/Staff/salesman.cs
@@ -0,0 +1,21 @@
+﻿namespace marketing.Staff
+{
+    public class salesman
+    {
+        private int id;
+        private string name;
+        private string lastname;
+        private int code;
+        private string phone_number;
+        private string mobile;
+        private string address;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Lastname { get => lastname; set => lastname = value; }
+        public int Code { get => code; set => code = value; }
+        public string Phone_number { get => phone_number; set => phone_number = value; }
+        public string Mobile { get => mobile; set => mobile = value; }
+        public string Address { get => address; set => address = value; }
+    }
+}
