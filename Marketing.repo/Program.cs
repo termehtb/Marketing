0949429035fbdcc73168ca167ad82c943d612cd3
termeh <termehtabatabaee@yahoo.com>
2021-07-07 13:56:47 +0430

    added marketing.repo file renamed model project

---
+++ Marketing.repo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marketing.repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
