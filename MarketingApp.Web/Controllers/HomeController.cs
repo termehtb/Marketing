a5ed97145b778c561ea33c5acab73c3a8d6d962d
termeh <termehtabatabaee@yahoo.com>
2021-07-08 11:38:39 +0430

    bug fixed

--- MarketingApp.Web/Controllers/HomeController.cs
+++ MarketingApp.Web/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 
 namespace MarketingApp.Web.Controllers
 {
-{
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
