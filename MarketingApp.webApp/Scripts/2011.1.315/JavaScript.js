f6c106ea45582649fdeb4268eaa529994b87813f
termeh <termehtabatabaee@yahoo.com>
2021-07-19 15:06:11 +0430

    seperate the script from html file

---
+++ MarketingApp.webApp/Scripts/2011.1.315/JavaScript.js
@@ -0,0 +1,27 @@
+﻿ type="text/javascript"
+    function myfunction() {
+        alert(2);
+        $.ajax({
+        type: 'POST',
+            cache: false,
+            url: '/Home/createSalesperson',
+            dataType: 'json',
+            data: {
+        name: $("#txtname").val(),
+                lastname: $("#txtlastname").val(),
+                code: parseInt($("#txtcode").val()),
+                commission: parseInt($("#txtcommission").val()),
+                phone: $("#txtphone").val(),
+                mobile: $("#txtmobile").val(),
+                address: $("#txtaddress").val()
+
+            },
+            error: function () {
+        alert('error')
+    },
+            success: function (result) {
+        alert(1);
+
+            }
+        });
+    }
